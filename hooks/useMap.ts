577b34654254389d2a5c67d5546c4654275011da
seungyun-Park <tmdrbs0925@gmail.com>
2024-05-03 13:39:10 +0900

    section3

--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react';
 import useSWR, { mutate } from 'swr';
 import type { Coordinates } from '../types/store';
 import type { NaverMap } from '../types/map';
-
+//
 export const INITIAL_CENTER: Coordinates = [37.5262411, 126.99289439];
 export const INITIAL_ZOOM = 10;
 
